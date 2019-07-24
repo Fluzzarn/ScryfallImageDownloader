ea5c29875d16a88e9790a969ae2a577039aabf9f
fluzzarn <fluzzarn@gmail.com>
2019-07-24 19:46:05 -0400

    Basic working app

--- Scryfall-Image-Downloader/Program.cs
+++ Scryfall-Image-Downloader/Program.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Scryfall_Image_Downloader
@@ -10,6 +14,53 @@ namespace Scryfall_Image_Downloader
     {
         static void Main(string[] args)
         {
+            String textFile = args[0];
+            Console.WriteLine(string.Format(@"Reading {0} for card names", textFile));
+            if (!Directory.Exists("./output"))
+            {
+                Directory.CreateDirectory("./output");
+            }
+
+            var lines = File.ReadAllLines(textFile);
+            foreach (string line in lines)
+            {
+                string cardName = line;
+                if (line.Length == 0) continue;
+                if (char.IsDigit(line[0]))
+                {
+                    cardName = line.Substring(line.IndexOf(' ')+1);
+                }
+                Console.WriteLine(String.Format("Getting image for {0}",cardName));
+                if (File.Exists(String.Format("./output/{0}.png", cardName)))
+                {
+                    Console.WriteLine("Found image to already exist locally, skipping");
+                    continue;
+                }
+                String uriCardName = Uri.EscapeUriString(cardName);
+                String url = JsonConvert.DeserializeObject<RootObject>(Get(String.Format("https://api.scryfall.com/cards/named?fuzzy={0}", uriCardName))).image_uris.png;
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFile(new Uri(url), String.Format(@"./output/{0}.png",cardName));
+                }
+                Thread.Sleep(10);
+            }
+
+            Console.WriteLine("Done, images downloaded to: " + Path.Combine(Directory.GetCurrentDirectory(),"output"));
+            Console.WriteLine("Press any key to exit");
+            Console.Read();
+        }
+
+        public static string Get(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
         }
     }
 }
